54bfb915b1740cfd6917ac5e14e4bbb1b0f882dc
董晓勇 <2627105433@qq.com>
2021-01-25 20:57:37 +0800

    SpringCloud整合zookeeper

---
+++ cloud_provider_payment8004/src/main/java/com/xiaoyong/springcloud/CloudProvider8004Application.java
@@ -0,0 +1,19 @@
+package com.xiaoyong.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * Create By dongxiaoyong on /2021/1/25
+ * description: 启动类
+ *
+ * @author dongxiaoyong
+ */
+@SpringBootApplication
+@EnableDiscoveryClient //该注解用于向使用consul或者zookeeper作为注册中心时注册服务
+public class CloudProvider8004Application {
+    public static void main(String[] args) {
+        SpringApplication.run(CloudProvider8004Application.class,args);
+    }
+}
---
+++ cloud_provider_payment8004/src/main/java/com/xiaoyong/springcloud/controller/PaymentController.java
@@ -0,0 +1,31 @@
+package com.xiaoyong.springcloud.controller;
+
+import io.swagger.annotations.Api;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
+
+/**
+ * Create By dongxiaoyong on /2021/1/25
+ * description: 支付controller
+ *
+ * @author dongxiaoyong
+ */
+@Slf4j
+@RestController
+@RequestMapping(value = "/payment")
+@Api(tags = "支付模块")
+public class PaymentController {
+
+    @Value("${server.port}")
+    private String serverPort;
+
+    @GetMapping(value = "/zk")
+    public String paymentZk() {
+        return "SpringCloud with zookeeper: " + serverPort + "\t" + UUID.randomUUID().toString();
+    }
+}
