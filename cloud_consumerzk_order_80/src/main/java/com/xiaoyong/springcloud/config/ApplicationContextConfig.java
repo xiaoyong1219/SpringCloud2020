a62024079dff3b9bc5e6dc1fff2a3832a4f30fa4
Dongxiaoyong <31543982+xiaoyong1219@users.noreply.github.com>
2021-01-28 00:10:55 +0800

    @LoadBalanced //若不加此注解，使用Ribbon做客户端负载均衡，服务提供者使用服务名，客户端报错， java.net.UnknownHostException

--- cloud_consumerzk_order_80/src/main/java/com/xiaoyong/springcloud/config/ApplicationContextConfig.java
+++ cloud_consumerzk_order_80/src/main/java/com/xiaoyong/springcloud/config/ApplicationContextConfig.java
@@ -1,5 +1,6 @@
 package com.xiaoyong.springcloud.config;
 
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
@@ -14,6 +15,7 @@ import org.springframework.web.client.RestTemplate;
 public class ApplicationContextConfig {
 
     @Bean
+    @LoadBalanced //若不加此注解，使用Ribbon做客户端负载均衡，服务提供者使用服务名，客户端报错， java.net.UnknownHostException
     public RestTemplate getTemplate(){
         return new RestTemplate();
     }
